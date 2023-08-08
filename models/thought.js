c73d993a12bd6cba9a6d491da3c652931f14f27b
Sam <sam.stikeleather@gmail.com>
2023-08-08 17:27:40 -0400

    thought.js

---
+++ models/thought.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const reactionSchema = require('./reaction');
+
+console.log('Importing Reaction schema');
+
+const thoughtSchema = new mongoose.Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+console.log('Defining Thought schema');
+
+const Thought = mongoose.model('Thought', thoughtSchema);
+
+console.log('Creating Thought model');
+
+module.exports = Thought;
